3408a3068fecfa66d2011761cd2f7a1cd37c9a2a
drrobb04 <drrobb04@gmail.com>
2021-08-23 15:59:10 -0600

    reorg of scrape py file

--- scrape_mars.py
+++ scrape_mars.py
@@ -3,8 +3,9 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 from splinter import Browser
 
-def news(browser):
-    #code for scraping
+def scrape():
+    
+    #code for news scraping
     browser = Browser('chrome', **executable_path, headless=False)
     scrape_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     browser.visit(scrape_url)
@@ -14,10 +15,9 @@ def news(browser):
     top_article = articles[0]
     news_title = top_article.find(class_ = "content_title").text
     news_graph = top_article.find(class_ = "article_teaser_body").text
-    return news_title, news_graph
-
-def image(browser):
-    #code for scraping
+    browser.quit()
+    
+    #code for jpl scraping
     browser = Browser('chrome', **executable_path, headless=False)
     scrape_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
     browser.visit(scrape_url)
@@ -27,11 +27,24 @@ def image(browser):
     subset = image.find("a")
     reference = subset["href"]
     jpl_url = f"https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{reference}"
-    return jpl_url
-
-def facts():
-    #code for scraping
+    browser.quit()
+    
+    #code for facts scraping
     mars_facts_df = pd.read_html("https://space-facts.com/mars/")[0]
-    return mars_facts_df.to_html()
+    mars_table = mars_facts_df.to_html()
+    
+    # Dictionary build
+    scrape_results = {
+        "news_title" : news_title,
+        "news_paragraph" : news_graph,
+        "featured_image" : jpl_url,
+        "facts" : mars_table,
+    }
+    
+    return scrape_results
+
+
+if __name__ == "__main__":
+    scrape.run(debug=True)
 
 
