2534ea4c57c7cf55f96ddc5e8285cdb414fb3f92
drrobb04 <drrobb04@gmail.com>
2021-08-23 17:53:33 -0600

    updates. first successful test

--- app.py
+++ app.py
@@ -12,25 +12,24 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_scrape_db")
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
-
     # Find one record of data from the mongo database
-    mars_run = mongo.db.mars_run.find_one()
+    mars = mongo.db.mars.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_run=mars_run)
+    return render_template("index.html", mars=mars)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():
-    mars_run = mongo.db.mars_run
+def scrape_it():
+    mars = mongo.db.mars
     mars_info = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mars_run.update({}, mars_info, upsert=True)
+    mars.update({}, mars_info, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -6,6 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape():
     
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    
     #code for news scraping
     browser = Browser('chrome', **executable_path, headless=False)
     scrape_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
